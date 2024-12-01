8612eddcf9fbf8a6d9cfb585f3ff1414de41cc0a
Eric <emoolenbeek@gmail.com>
2024-12-01 14:47:38 -0500

    modify nav

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -4,11 +4,6 @@
 
 <nav class="navigation border-b">
 	<div class="container flex h-14 items-center px-4 max-w-7xl mx-auto">
-		<h1 class="text-lg font-semibold"><a href="/">Website</a></h1>
-		
-		<div class="flex items-center gap-4 ml-auto">
-			<Button variant="ghost" href="/about">About</Button>
-			<Button variant="ghost" href="/contact">Contact</Button>
-		</div>
+		<h1 class="text-lg font-semibold"><a href="/">Metronome</a></h1>
 	</div>
 </nav>
